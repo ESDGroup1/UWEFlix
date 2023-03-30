a64420d546fb9234c1581edc24a6174b1c048ac5
samparchao <58604331+samparchao@users.noreply.github.com>
2023-03-30 19:50:59 +0100

    Filter added & fixes
    - Implemented Filter for showings by date and error message if a date has no showing
    - Added Super User back
    - Fixed footer issue where media would overwrite current open page

--- Index/views.py
+++ Index/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import logout
 from django.shortcuts import get_object_or_404, redirect, render
 from CinManager.models import Club, ClubRep, Film, Screen, Showing
+from datetime import datetime
 
 
 # Create your views here.
@@ -12,18 +13,43 @@ def is_member_CinemaManager(request):
         isCinManager = 0
     return isCinManager #returns value to orignal function
 
+from datetime import datetime
+
 def home(request):
     isCinManager = is_member_CinemaManager(request)
-
-    print("MANAGER=",isCinManager)
-    films = Film.objects.all() #gathring all films
+    
+    # check if a date has been submitted in the form
+    date = request.GET.get('date')
+    if date:
+        try:
+            date = datetime.strptime(date, '%Y-%m-%d').date()
+            # get all showings for the selected date
+            showings = Showing.objects.filter(date=date)
+            if not showings:
+                # show message if there are no showings on the selected date
+                messages.warning(request, "Sorry, there are no showings on this date.")
+                films = []
+            else:
+                # get the films that have showings on the selected date
+                films = Film.objects.filter(showing__in=showings).distinct()
+        except ValueError:
+            messages.error(request, "Invalid date format.")
+            # show all films if there was an error with the submitted date
+            films = Film.objects.all()
+    else:
+        # show all films by default
+        films = Film.objects.all()
+        
     for film in films:
-        showings = Showing.objects.filter(film=film) #checking each film and checking if it has showings 
+        # check if each film has showings
+        showings = Showing.objects.filter(film=film)
         if not showings:
             film.has_showings = False
         else:
             film.has_showings = True
-    return render(request, 'UWEFlix/home.html', {'films': films, 'iscinmanager': isCinManager}) #send necessary info
+            
+    return render(request, 'UWEFlix/home.html', {'films': films, 'iscinmanager': isCinManager})
+
 
 def film_detail_view(request, film_id):
     isCinManager = is_member_CinemaManager(request)
